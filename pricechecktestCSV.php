c99325363be6148cbfab519c94814586bb1c14a2
Charles Hwang <chwang@Charles13Retina.home>
2015-03-01 17:58:32 -0500

    Version 2
    Broke up code into functions. Using a batch lookup to speed up queries
    to iTunes.

--- pricechecktestCSV.php
+++ pricechecktestCSV.php
@@ -2,100 +2,124 @@
 	Enter the location of your CSV file into $csvFile.
 	Should be just a one column CSV file with an iTunes ID on each row.
 	
-	Now with CSV support!
-	Optimized by using batch lookup - iTunes has some floodgates for handling individual queries it seems
+	Broke up code into functions.
+	I also learned about how globals work in PHP. Lesson learned.
 -->
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
 <head>
-<!-- Initialize some things in PHP -->
-<?php
-		$csvFile = ''; //enter a CSV file location here
-		$database = fopen($csvFile, 'r');
+	<!-- Initialize some things in PHP -->
+	<?php
 		date_default_timezone_set('EST');
-?>
+	?>
     <title>iTunes Price Checker for <?= date('l j F Y') ?> </title>
 </head>
 
 <body>
 	<table>
 <?php
-
+	//globals
+	$csvFile = "/Users/chwang/GitHub Projects/iTunesPriceTracker/IDs.csv"; //enter a CSV file location here
+	
 	$start_time = microtime(true); //!benchmarking
 
-	//Appending ID's into one big string
-	if (($handle = $database) != FALSE) {
-		$count = 0;
-		//?instead of 100, use some kind of const
-		//this reads any column headers, if you decide to use column headers in your CSV file
-/* 		while (($line = fgetcsv($handle, 100, ",")) != FALSE) { */
-			//?instead of 50, use a const
-			while ($count < 50 && ($line = fgetcsv($handle, 100, ",")) != FALSE) {
-				if ($line[0] != '0') {
-					$ids = $ids . "," . $line[0];
-					$count++;
-				}
-				/*
-				//Making sure 0's aren't read in.
-				else {
-					echo "Invalid ID: " . $line[0] . "<br>";
-				}
-				*/
-			}
-/* 		} */
-		fclose($handle);
-	}
-	else {
-		echo "Failed to open file.";
-	}
-	
-	//Long ID string
-	$ids = substr($ids, 1);
-
-	//echo $ids, "<br>"; //Making sure my IDs are appended, correctly
-	//echo $count, "<br>"; //Making sure I read in the right number of valid values
-
-	//used cURL to keep iTunes Search API happy with my scripted search
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "https://itunes.apple.com/lookup?id=$ids"); //this queries all of the ID's all at once
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	$content = curl_exec($ch); //JSON received
-	curl_close($ch);
-	
-	//results array turned into a PHP array
-	$content = json_decode($content);	
-	//var_dump($parsed); //Making sure $parsed is right
-	
-	//if the item exists by comparing object's resultCount value
-	if ($content->resultCount == $count) {
-		//echo $content->resultCount . "<br>"; //number of results returned from iTunes
-		for ($i = 0; $i < $count; $i++) {
-			$parsed = $content->results[$i];
-			
-			//Name, art, price
-			$name = $parsed->collectionName;
-			$art = $parsed->artworkUrl100;
+	/**
+	 * opens up a CSV file with a list of iTunes items and creates a string of IDs to enter into iTunes Search API lookup
+	 * $numIDs - number of IDs to put in the string
+	 * $readerLength - fgetcsv()'s length
+	 * returns a string of $numIDs of IDs
+	 */
+	function createIDsString($numIDs, $readerLength) {
+		global $csvFile;
 		
-			//if-then for HD collection pricing where applicable		
-			if (is_null($parsed->collectionHdPrice))
-				$price = $parsed->collectionPrice;
-			else
-				$price = $parsed->collectionHdPrice;
-			
-			?>
-			<tr>
-				<img src="<?= $art ?>"/> <br>
-				Name: <a href="https://itunes.apple.com/lookup?id=<?=$id?>"><?= $name ?></a> <br>
-				Price: $<?= $price ?> <br>
-				<br>
-			</tr>
-			<?
+		$database = fopen($csvFile, 'r');
+		if ($database != FALSE) {
+			$count = 0;
+			$prevLine = 0;
+			//?instead of 100, use some kind of const
+			//this reads any column headers, if you decide to use column headers in your CSV file
+/* 			while (($line = fgetcsv($handle, 100, ",")) != FALSE) { */
+			//?instead of 50, use a const
+				while (($count < $numIDs) && (($line = fgetcsv($database, $readerLength, ",")) != FALSE)) {
+					if ($line[0] != '0') { //iTunes Search API returns 0 for nonexistent records, using 0 in my CSV for testing sake
+						$ids = $ids . "," . $line[0];
+						$count++;
+						$prevLine = $line[0];
+					}
+					/*
+					//Making sure 0's aren't read in.
+					else {
+						echo "Invalid ID: " . $line[0] . "<br>";
+					}
+					*/
+				}
+				$count = 0;
+/* 			} */
+			fclose($database);
 		}
+		else {
+			echo "Failed to open file.";
+		}
+		
+		//Long ID string
+		$ids = substr($ids, 1); //take off that last comma
+		//echo $ids, "<br>"; //!debug
+		return $ids;
+	}
+
+	/**
+	 * use cURL (Client URL Library) to get JSON objects from iTunes Search API
+	 * $concatID - ID's to put into iTunes Search API URL
+	 * returns JSON objects as an array
+	 */
+	function getJSONObjects($concatID) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "https://itunes.apple.com/lookup?id=$concatID");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$content = curl_exec($ch); //JSON received
+		curl_close($ch);
+		
+		$content = json_decode($content); //decodes JSON string into an array
+		
+		return $content;
+	}
+	
+	/**
+	 * creates the info card of a given iTunes ID
+	 * $JSONResult - JSON objects to sort and break
+	 */
+	function createInfo($JSONResult) {		
+		$name = $JSONResult->collectionName;
+		$art = $JSONResult->artworkUrl100;
+		//if-then for HD collection pricing where applicable
+		if (is_null($JSONResult->collectionHdPrice))
+			$price = $JSONResult->collectionPrice;
+		else
+			$price = $JSONResult->collectionHdPrice;
+		
+		?>
+		<div class = "iTunesItem">
+			<img src="<?= $art ?>"/> <br>
+			<a href="https://itunes.apple.com/lookup?id=<?=$id?>"><?= $name ?></a> <br>
+			$<?= $price ?> <br>
+			<br>
+		</div>
+		<br>
+		<?				
+	}
+	
+	
+	// Where the action happens...
+	$IDList = createIDsString(50, 100);
+	$searchResults = getJSONObjects($IDList);
+	for ($i = 0; $i < $searchResults->resultCount; $i++) {
+		//echo $i + 1, ": <br>";//!debug
+		createInfo($searchResults->results[$i]);
 	}
 
 	$end_time = microtime(true); //!benchmarking
-	echo 'Total time to retrieve: ', $end_time-$start_time, ' s', "<br>"; //!benchmarking
+	echo 'Total time to retrieve: ', $end_time - $start_time, ' s', "<br>"; //!benchmarking
 	
 ?>
 	</table>
