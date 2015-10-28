0146c7714a990532ac68b61518c371f27e0eeb30
Charles Hwang <charles.hwang@icloud.com>
2015-10-28 00:55:05 -0400

    Updated URLtoID
    Updated regex

--- URLtoID/javascript/URLtoID.js
+++ URLtoID/javascript/URLtoID.js
@@ -1,6 +1,6 @@
 function transform() {
     var url = document.getElementById("url").value; //getting URL from text field
-    var pattern = /id(.+)\?/; //regex pattern
+    var pattern = /id(.+)/; //regex pattern
 	var results = pattern.exec(url); //results of regex
 	var id = results[1]; //results[1] holds the ID
 	var idField = document.getElementById("id"); //getting ID text field
